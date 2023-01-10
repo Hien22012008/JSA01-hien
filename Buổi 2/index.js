3f893db6f5ebeae7c55c8f9acdbd615b5e485c6b
Hien22012008 <xuanhiennguyen34@gmail.com>
2023-01-10 20:35:14 +0700

    buoi2

---
+++ Buổi 2/index.js
@@ -0,0 +1,22 @@
+// for(var a = 0 ; a <= 10; a++){
+//     console.log( a )
+// }
+
+// var a = 3;
+// while (a < 5) {
+//     console.log("lap lai " + a)
+//     a++
+// }
+
+// do {
+//     console.log("lap lai " + a)
+//     a++ 
+// }while(a < 5)
+
+var hocsinh = {
+    ten: "Hien",
+    tuoi: 14,
+    truong: "Lac Hong",
+    lop: 9/7,
+};
+console.log("Ten minh la " + hocsinh.ten);
